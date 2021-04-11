7fcace203c98b3fd6aafdb7f10def3a994dcb0df
mohammadmmoniri <mmmoniri77@gmail.com>
2021-04-11 22:49:57 +0430

    add count to linkermodel

--- linker/models.py
+++ linker/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-class Link(models.Model):
+class LinkModel(models.Model):
     owner = models.ForeignKey(to=get_user_model(), on_delete=models.DO_NOTHING, null=True, blank=True)
     title = models.CharField(max_length=125)
     link = models.CharField(max_length=255)
     shortcut = models.CharField(max_length=10)
     description = models.TextField(blank=True, null=True)
+    click_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
