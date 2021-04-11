2929da25765abe019144b8c4b29abf19673b1e9b
mohammadmmoniri <mmmoniri77@gmail.com>
2021-04-11 22:48:54 +0430

    add anonymouslinkcreate form

---
+++ linker/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import LinkModel
+
+
+URL_REGEX = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+
+class AnonymousLinkForm(forms.ModelForm):
+    link = forms.RegexField(regex=URL_REGEX, widget=forms.TextInput(attrs={'class' : 'form-control', "aria-describedby":"button-addon2"}))
+
+    class Meta:
+        model = LinkModel
+        fields = ["link"]
+        fields_required = ['link']
+
