d0da6858577c772f88e5abdddb21b1afb0b28e47
mohammadmmoniri <mmmoniri77@gmail.com>
2021-04-11 22:50:44 +0430

    add post func to homeview

--- linker/views.py
+++ linker/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
-
+from .forms import AnonymousLinkForm
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'html/home.html')
+        form = AnonymousLinkForm() 
+        return render(request, 'html/home.html', {'form': form})
+
+    def post(self, request):
+        form = AnonymousLinkForm(request.POST)
+        if form.is_valid():
+            message = {
+                "type": "success",
+                "data": "ur link created successfully"
+            }
+            return render(request, 'html/home.html', {"form": form, "message":message})
+        else:
+            message = {
+                "type": "danger",
+                "data": "bad link"
+            }
+            return render(request, 'html/home.html', {"form": form, "message":message})
