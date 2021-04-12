a44d8e3abb39eceb0d0d97a62cd7f5825d6765aa
mohammadmmoniri <mmmoniri77@gmail.com>
2021-04-12 23:51:48 +0430

    create test for get_orgina_link

--- linker/tests.py
+++ linker/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import LinkModel
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class LinkTestCase(TestCase):
+    def setUp(self):
+        LinkModel.objects.create(title="google", link="www.google.com", shortcut="xXse3")
+        user = get_user_model().objects.first()
+        LinkModel.objects.create(owner=user, title="yahoo", link="www.yahoo.com", shortcut="yahoo")
+    
+    def test_get_orginal_link(self):
+        google = LinkModel.get_orginal_link("xXse3")
+        self.assertEqual(google, "www.google.com")
+        yahoo = LinkModel.get_orginal_link(shortcut="yahoo")
+        self.assertEqual(yahoo, "www.yahoo.com")
