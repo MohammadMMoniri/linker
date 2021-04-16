9c1ff502381535559cc00abf721b4ce898ec452e
mohammadmmoniri <mmmoniri77@gmail.com>
2021-04-16 17:36:48 +0430

    delete tests

--- linker/tools.py
+++ linker/tools.py
@@ -1,8 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import datetime
 
 URL_REGEX = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+
+
+def base64(num: int):
+    result = ""
+    while True:
+        remainder = num % 64
+        num = num // 64
+
+        if remainder < 9:
+            result += str(remainder)
+        elif remainder < 36:
+            result += chr(remainder + 55)
+        else:
+            result +=chr(remainder + 60)
+        if num == 0:
+            break
+    return result
 
 
 class Link:
@@ -26,8 +44,13 @@ class Link:
         link_page_content = requests.get(link).content
         soup = BeautifulSoup(link_page_content, 'html.parser')
         return str(soup.title.string)
+    
+    def create_shortcut(self):
+        link = self.link
+        form = "%d%m%H%M%S%f"
+        time = datetime.datetime.now()
+        year = str(int(time.strftime("%Y")) - 2020)
+        time = year + time.strftime(form)
+        time = int(time)
+        return base64(time)
 
-
-link = Link(link="https://www.google.com/")
-print(type(link.get_title()))
-print(Link.link_normalize("https://www.google.com/"))
