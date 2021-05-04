117024908fbd1e21bff337506bd9d6d12823127a
mohammadmmoniri <mmmoniri77@gmail.com>
2021-05-05 00:17:46 +0430

    add accounts urls to config setting

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+# from django.contrib.auth.urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('linker.urls') ),
+    path('', include('linker.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
